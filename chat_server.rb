a7aa4a1efd816e5b4348b88ab6c6100ec7ee7f9d
Ashish Rao <ashishrao2598@gmail.com>
2023-01-07 10:56:53 +0530

    Refactor code into seperate class

--- chat_server.rb
+++ chat_server.rb
@@ -5,6 +5,7 @@ require './member'
 class ChatServer
 
   attr_reader :tcp_server, :startup_message, :members
+
   def initialize
     @tcp_server = TCPServer.new(2000)
     @startup_message = "Server running on port 2000"
@@ -16,31 +17,13 @@ class ChatServer
       tcp_socket = tcp_server.accept
       Thread.new(tcp_socket) do |socket|
         startup_message
-        username = get_username(socket)
-        member = Member.new(username, socket)
-        member.welcome_from(members)
-        members.add(member)
-        members.broadcast("joined the chat", member)
-        # socket.close
-
+        member = members.register(socket)
         begin
-          loop do
-            message = socket.readline
-            members.broadcast(message, member)
-          end
+          members.start_listening_to(member)
         rescue EOFError
-          socket.close
-          members.remove(member)
-          members.broadcast("[Left]", member)
+          members.disconnect(member)
         end
       end
     end
-  end
-
-  private
-
-  def get_username(socket)
-    socket.print("What's your name?")
-    socket.gets.chomp
   end
 end
--- members.rb
+++ members.rb
@@ -23,4 +23,34 @@ class Members
       receiver.socket.puts("> #{sender.username}: #{message}")
     end
   end
+
+  def register(socket)
+    username = get_username(socket)
+    member = Member.new(username, socket)
+    member.welcome_from(self)
+    add(member)
+    broadcast("[Joined]", member)
+
+    member
+  end
+
+  def disconnect(member)
+    member.socket.close
+    remove(member)
+    broadcast("[Left]", member)
+  end
+
+  def start_listening_to(member)
+    loop do
+      message = member.socket.readline
+      broadcast(message, member)
+    end
+  end
+
+  private
+
+  def get_username(socket)
+    socket.print("What's your name?")
+    socket.gets.chomp
+  end
 end
